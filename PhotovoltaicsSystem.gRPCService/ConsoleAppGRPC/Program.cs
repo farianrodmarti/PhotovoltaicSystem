ae4836b40a383bac4697a34bfddcdd631896fd4f
Anita0599 <anirodroig@gmail.com>
2025-06-25 08:36:13 +0200

    gRPCService

---
+++ PhotovoltaicsSystem.gRPCService/ConsoleAppGRPC/Program.cs
@@ -0,0 +1,10 @@
+﻿using Grpc.Net.Client;
+using GrpcService;
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7183");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+                  new HelloRequest { Name = "Developers" });
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
